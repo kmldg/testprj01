a53c89aa8665a96397e4bc1af069acf15f9e7eb3
DONGGIL <kmldg7@naver.com>
2016-05-19 23:51:40 +0900

    test commit #2

--- src/gilsTest/test.java
+++ src/gilsTest/test.java
@@ -4,4 +4,7 @@ package gilsTest;
  * Created by DONGGIL on 2016-05-19.
  */
 public class test {
+    public static void main(String args[]) {
+        System.out.println("Just git clone test");
+    }
 }
